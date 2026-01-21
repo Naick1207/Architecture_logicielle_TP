79be56e89b1b61343b24caa097de4c865fcf1979
Naick1207 <naick.cher@etu.univ-orleans.fr>
2026-01-21 14:52:49 +0100

    Exercice 3

--- todo/views.py
+++ todo/views.py
@@ -6,12 +6,22 @@ from .models import tasks
 def get_tasks():
     public_tasks = []
     for task in tasks:
-        public_tasks.append(task)
+        public_tasks.append(make_public_task(task))
     return jsonify({'tasks': public_tasks})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
 def get_task(task_id):
     for task in tasks:
         if task['id'] == task_id:
-            return jsonify({'task': task})
+            return jsonify({'task': make_public_task(task)})
     return {'error': 'Not found'}, 404
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'],
+                                        _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
